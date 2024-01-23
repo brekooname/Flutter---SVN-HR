40c131746ddd18a71db463269cb0ad7fceb0e129
Wasim Anabtawi <96519449+WasimAnabtawi@users.noreply.github.com>
2024-01-23 09:23:00 +0200

    Delete lib directory

--- lib/utilities/hex_color.dart
+++
@@ -1,9 +0,0 @@
-class HexColor {
-  static int getColorFromHex(String hexColor) {
-    hexColor = hexColor.toUpperCase().replaceAll("#", "");
-    if (hexColor.length == 6) {
-      hexColor = "FF" + hexColor;
-    }
-    return int.parse(hexColor, radix: 16);
-  }
-}
